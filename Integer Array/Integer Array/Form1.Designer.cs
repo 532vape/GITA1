0e072532565917709f71d7dc2b2fc823fb0e4b67
532vape <davidhyuntek@gmail.com>
2023-03-29 20:12:19 -0700

    Add files via upload

---
+++ Integer Array/Integer Array/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Integer_Array
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnrandom = new System.Windows.Forms.Button();
+            this.txtquantity = new System.Windows.Forms.TextBox();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblavg = new System.Windows.Forms.Label();
+            this.lbloutput = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnrandom
+            // 
+            this.btnrandom.Location = new System.Drawing.Point(241, 78);
+            this.btnrandom.Name = "btnrandom";
+            this.btnrandom.Size = new System.Drawing.Size(85, 29);
+            this.btnrandom.TabIndex = 1;
+            this.btnrandom.Text = "Generate";
+            this.btnrandom.UseVisualStyleBackColor = true;
+            this.btnrandom.Click += new System.EventHandler(this.btnrandom_Click);
+            // 
+            // txtquantity
+            // 
+            this.txtquantity.Location = new System.Drawing.Point(241, 45);
+            this.txtquantity.Name = "txtquantity";
+            this.txtquantity.Size = new System.Drawing.Size(100, 23);
+            this.txtquantity.TabIndex = 2;
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Location = new System.Drawing.Point(20, 45);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(85, 29);
+            this.btnsearch.TabIndex = 3;
+            this.btnsearch.Text = "Search";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            this.btnsearch.Click += new System.EventHandler(this.btnsearch_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblavg
+            // 
+            this.lblavg.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblavg.Location = new System.Drawing.Point(20, 114);
+            this.lblavg.Name = "lblavg";
+            this.lblavg.Size = new System.Drawing.Size(171, 31);
+            this.lblavg.TabIndex = 7;
+            // 
+            // lbloutput
+            // 
+            this.lbloutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbloutput.Location = new System.Drawing.Point(17, 44);
+            this.lbloutput.Name = "lbloutput";
+            this.lbloutput.Size = new System.Drawing.Size(213, 218);
+            this.lbloutput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(17, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 30);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Average Attempts To Find";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnsearch);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lblavg);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(379, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 167);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Array";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbloutput);
+            this.groupBox2.Controls.Add(this.btnrandom);
+            this.groupBox2.Controls.Add(this.txtquantity);
+            this.groupBox2.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(361, 292);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Integer Array";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 323);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Number Array";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnrandom;
+        private System.Windows.Forms.TextBox txtquantity;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblavg;
+        private System.Windows.Forms.Label lbloutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Integer Array/Integer Array/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Integer_Array
+{
+
+    public partial class Form1 : Form
+    {/*
+        This program generates a series of numbers in two arrays. The first, "search array" generates a 
+        number and tries to find it in an array of 5000 numbers
+        The second "Integer Array" generates a (user-generated) quantity of random numbers and finds the avg, max, and min value of the
+        numbers
+        */
+        System.Random r = new System.Random((int)System.DateTime.Now.Ticks);
+        int[] numberarray;
+        int[] searcharray;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnrandom_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int howmany = int.Parse(txtquantity.Text);
+                int maxvalue = 0;
+                int minvalue = 5000;
+                int average = 0;
+                numberarray = new int[howmany];
+                string message = "";
+                string message2 = "";
+                string message3 = "";
+
+                for (int i = 0; i < numberarray.Length; i++)
+                {
+                    //fill the array
+                    int myrandom = r.Next(1, 5001);
+                    numberarray[i] = myrandom;
+                    average += numberarray[i] / howmany;
+                }
+
+                for (int i = 0; i < numberarray.Length; i++)
+                {
+                    //detect the greatest value
+                    if (numberarray[i] > maxvalue)
+                    {
+                        maxvalue = numberarray[i];
+                    }
+
+                    //detect the min value
+                    if (numberarray[i] <= minvalue)
+                    {
+                        minvalue = numberarray[i];
+
+                        if (numberarray[i] == 0)
+                        {
+                            minvalue = 0;
+                            minvalue = 0;
+                        }
+                    }
+                }
+
+
+                //output to label
+                for (int i = 0; i < numberarray.Length; i++)
+                {
+                    message += numberarray[i] + "\n";
+                }
+
+                message += "MaxValue: " + maxvalue;
+                message2 += "MinValue: " + minvalue;
+                message3 += "AvgValue: " + average;
+                lbloutput.Text = message + "\n" + message2 + "\n" + message3;
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void btnsearch_Click(object sender, EventArgs e)
+        {
+            int howMany = 5000;
+            searcharray = new int[howMany];
+            int randomnum = 0;
+            int whatnumber = 0;
+            int averagenum = 0;
+
+            for (int i = 0; i < searcharray.Length; i++)
+            {
+                
+                for (int N = 0; N < searcharray.Length; N++)
+                {
+                    //fill the array
+                    int randominteger = r.Next(1, 5001);
+                    searcharray[N] = randominteger;
+                }
+
+                randomnum = r.Next(1, 5001);
+
+                for (int m = 0; m < searcharray.Length; m++)
+                {
+                    whatnumber++;
+                    if (searcharray[m] == randomnum)
+                    {
+                        //stop the for loop once found
+                        break;
+                    }
+                }
+            }
+            //output the average to the label
+            averagenum = whatnumber / 5000;
+            lblavg.Text = averagenum.ToString();
+        }
+       private void timer1_Tick(object sender, EventArgs e)
+        {
+ 
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblattempts_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
