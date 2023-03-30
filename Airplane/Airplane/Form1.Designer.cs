b1902d339e09bf4ecec8e5ad7035767c7b725646
532vape <davidhyuntek@gmail.com>
2023-03-29 19:54:58 -0700

    Add files via upload

---
+++ Airplane/Airplane/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Airplane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picairplane = new System.Windows.Forms.PictureBox();
+            this.pictarget = new System.Windows.Forms.PictureBox();
+            this.piczeroleft = new System.Windows.Forms.PictureBox();
+            this.piczeroright = new System.Windows.Forms.PictureBox();
+            this.picarizonaleft = new System.Windows.Forms.PictureBox();
+            this.picarizonaright = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.prgtarget = new System.Windows.Forms.ProgressBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.lbltimer = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picairplane)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictarget)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piczeroleft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piczeroright)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picarizonaleft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picarizonaright)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picairplane
+            // 
+            this.picairplane.Image = ((System.Drawing.Image)(resources.GetObject("picairplane.Image")));
+            this.picairplane.Location = new System.Drawing.Point(338, 87);
+            this.picairplane.Name = "picairplane";
+            this.picairplane.Size = new System.Drawing.Size(100, 50);
+            this.picairplane.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picairplane.TabIndex = 0;
+            this.picairplane.TabStop = false;
+            // 
+            // pictarget
+            // 
+            this.pictarget.Image = ((System.Drawing.Image)(resources.GetObject("pictarget.Image")));
+            this.pictarget.Location = new System.Drawing.Point(338, 347);
+            this.pictarget.Name = "pictarget";
+            this.pictarget.Size = new System.Drawing.Size(100, 50);
+            this.pictarget.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictarget.TabIndex = 1;
+            this.pictarget.TabStop = false;
+            // 
+            // piczeroleft
+            // 
+            this.piczeroleft.Image = ((System.Drawing.Image)(resources.GetObject("piczeroleft.Image")));
+            this.piczeroleft.Location = new System.Drawing.Point(778, 12);
+            this.piczeroleft.Name = "piczeroleft";
+            this.piczeroleft.Size = new System.Drawing.Size(10, 10);
+            this.piczeroleft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.piczeroleft.TabIndex = 2;
+            this.piczeroleft.TabStop = false;
+            this.piczeroleft.Visible = false;
+            // 
+            // piczeroright
+            // 
+            this.piczeroright.Image = ((System.Drawing.Image)(resources.GetObject("piczeroright.Image")));
+            this.piczeroright.Location = new System.Drawing.Point(778, 12);
+            this.piczeroright.Name = "piczeroright";
+            this.piczeroright.Size = new System.Drawing.Size(10, 10);
+            this.piczeroright.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.piczeroright.TabIndex = 3;
+            this.piczeroright.TabStop = false;
+            this.piczeroright.Visible = false;
+            // 
+            // picarizonaleft
+            // 
+            this.picarizonaleft.Image = ((System.Drawing.Image)(resources.GetObject("picarizonaleft.Image")));
+            this.picarizonaleft.Location = new System.Drawing.Point(778, 12);
+            this.picarizonaleft.Name = "picarizonaleft";
+            this.picarizonaleft.Size = new System.Drawing.Size(10, 10);
+            this.picarizonaleft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picarizonaleft.TabIndex = 4;
+            this.picarizonaleft.TabStop = false;
+            this.picarizonaleft.Visible = false;
+            // 
+            // picarizonaright
+            // 
+            this.picarizonaright.Image = ((System.Drawing.Image)(resources.GetObject("picarizonaright.Image")));
+            this.picarizonaright.Location = new System.Drawing.Point(778, 12);
+            this.picarizonaright.Name = "picarizonaright";
+            this.picarizonaright.Size = new System.Drawing.Size(10, 10);
+            this.picarizonaright.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picarizonaright.TabIndex = 5;
+            this.picarizonaright.TabStop = false;
+            this.picarizonaright.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // prgtarget
+            // 
+            this.prgtarget.Location = new System.Drawing.Point(12, 12);
+            this.prgtarget.Name = "prgtarget";
+            this.prgtarget.Size = new System.Drawing.Size(195, 30);
+            this.prgtarget.TabIndex = 6;
+            this.prgtarget.Value = 100;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // lbltimer
+            // 
+            this.lbltimer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbltimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltimer.Location = new System.Drawing.Point(706, 12);
+            this.lbltimer.Name = "lbltimer";
+            this.lbltimer.Size = new System.Drawing.Size(82, 69);
+            this.lbltimer.TabIndex = 7;
+            this.lbltimer.Text = "100";
+            this.lbltimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbltimer);
+            this.Controls.Add(this.prgtarget);
+            this.Controls.Add(this.picarizonaright);
+            this.Controls.Add(this.picarizonaleft);
+            this.Controls.Add(this.piczeroright);
+            this.Controls.Add(this.piczeroleft);
+            this.Controls.Add(this.pictarget);
+            this.Controls.Add(this.picairplane);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.picairplane)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictarget)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piczeroleft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piczeroright)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picarizonaleft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picarizonaright)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picairplane;
+        private System.Windows.Forms.PictureBox pictarget;
+        private System.Windows.Forms.PictureBox piczeroleft;
+        private System.Windows.Forms.PictureBox piczeroright;
+        private System.Windows.Forms.PictureBox picarizonaleft;
+        private System.Windows.Forms.PictureBox picarizonaright;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar prgtarget;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label lbltimer;
+    }
+}
+
---
+++ Airplane/Airplane/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Airplane
+{
+    public partial class Form1 : Form
+    {
+        private int time = 100;
+        List<PictureBox> bulletlist = new List<PictureBox>();
+        System.Random r = new System.Random((int)System.DateTime.Now.Ticks);
+        int bulletcount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            KeyEventArgs key = e;
+
+            if (key.KeyValue == 37)
+            {
+                pictarget.Left -= 10;
+                pictarget.Image = picarizonaleft.Image;
+                if (pictarget.Left <= 0)
+                {
+                    pictarget.Left = 0;
+                }
+            }
+
+            else if (key.KeyValue == 39)
+            {
+                pictarget.Left += 10;
+                pictarget.Image = picarizonaright.Image;
+
+                if (pictarget.Left >= 525)
+                {
+                    pictarget.Left = 525;
+                }
+            }
+
+            if (key.KeyValue == 32)
+            {
+               
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveenemy();
+            
+        }
+
+        private void moveprojectile()
+        {
+            int bombdrop = r.Next(1, 21);
+
+            if (bombdrop == 2 | bombdrop == 4 | bombdrop == 6 | bombdrop == 8 | bombdrop == 10 | bombdrop == 12 | bombdrop == 14 | bombdrop == 16 | bombdrop == 18 | bombdrop == 20)
+            {
+                if (bulletcount < 20)
+                {
+                    bulletlist.Add(new PictureBox());
+                    //place picbox on the form
+                    this.Controls.Add(bulletlist.ElementAt(bulletcount));
+                    bulletlist.ElementAt(bulletcount).Height = 10;
+                    bulletlist.ElementAt(bulletcount).Width = 10;
+                    bulletlist.ElementAt(bulletcount).Image = Image.FromFile("bobm.jpg", true);
+                    bulletlist.ElementAt(bulletcount).SizeMode = PictureBoxSizeMode.StretchImage;
+                    bulletlist.ElementAt(bulletcount).Left = picairplane.Left + picairplane.Width / 2;
+                    bulletlist.ElementAt(bulletcount).Top = picairplane.Bottom;
+                    bulletcount++;
+                }
+            }
+
+            for (int i = 0; i < bulletlist.Count; i++)
+            {
+                bulletlist.ElementAt(i).Top += 10;
+            }
+
+       
+            for (int j = 0; j < bulletlist.Count; j++)
+            {
+                try
+                {
+                    if (pictarget.Bounds.IntersectsWith(bulletlist.ElementAt(j).Bounds))
+                    {
+                        prgtarget.Value -= 10;
+                    }
+                }
+                catch
+                {
+                    timer1.Enabled = false;
+                    timer2.Enabled = false;
+                    timer3.Enabled = false;
+                    MessageBox.Show("Game Over!");
+                }
+
+            }
+        
+
+            for (int k = 0; k < bulletlist.Count; k++)
+            {
+                if (bulletlist.ElementAt(k).Top > 500)
+                {
+                    this.Controls.Remove(bulletlist.ElementAt(k));
+                    bulletlist.Remove(bulletlist.ElementAt(k));
+                    bulletcount--;
+
+                    if (bulletcount <= 0)
+                    {
+                        bulletlist = new List<PictureBox>();
+                        bulletcount = 0;
+                        break;
+                    }
+                }
+            }
+
+            
+        }
+
+        private void moveenemy()
+        {
+            int leftright = r.Next(1, 11);
+
+            if (leftright == 2 | leftright == 4 | leftright == 6 | leftright == 8 | leftright == 10)
+            {
+                picairplane.Left -= 10;
+            }
+
+            else if (leftright == 1 | leftright == 3 | leftright == 5 | leftright == 7 | leftright == 9)
+            {
+                picairplane.Left += 10;
+            }
+        }
+
+        private void timer()
+        {
+            time--;
+            for (int i = 0; i < bulletlist.Count; i++)
+            {
+                for (int k = 0; k < bulletlist.Count; k++)
+                {
+                    lbltimer.Text = time.ToString();
+                }
+
+                if (time <= 0)
+                {
+                    timer1.Enabled = false;
+                    timer2.Enabled = false;
+                    timer3.Enabled = false;
+                    MessageBox.Show("Game Over!");
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer();   
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            moveprojectile();
+        }
+    }
+}
