ac75f8e292f503ebec5fb241c8415513fdee7c0b
532vape <davidhyuntek@gmail.com>
2023-03-29 20:09:20 -0700

    Add files via upload

---
+++ FirePower/FirePower/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace FirePower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblspaceship = new System.Windows.Forms.Label();
+            this.lbltarget = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblspaceship
+            // 
+            this.lblspaceship.BackColor = System.Drawing.Color.Lime;
+            this.lblspaceship.Location = new System.Drawing.Point(345, 360);
+            this.lblspaceship.Name = "lblspaceship";
+            this.lblspaceship.Size = new System.Drawing.Size(100, 23);
+            this.lblspaceship.TabIndex = 0;
+            this.lblspaceship.Text = "Spaceship";
+            // 
+            // lbltarget
+            // 
+            this.lbltarget.BackColor = System.Drawing.Color.Red;
+            this.lbltarget.Location = new System.Drawing.Point(350, 214);
+            this.lbltarget.Name = "lbltarget";
+            this.lbltarget.Size = new System.Drawing.Size(100, 23);
+            this.lbltarget.TabIndex = 1;
+            this.lbltarget.Text = "target";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbltarget);
+            this.Controls.Add(this.lblspaceship);
+            this.Name = "Form1";
+            this.Text = "Fire Power";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblspaceship;
+        private System.Windows.Forms.Label lbltarget;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FirePower/FirePower/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirePower
+{
+    public partial class Form1 : Form
+    {
+        // a list to hold bullets
+        List<PictureBox> bulletlist = new List<PictureBox>();
+        int bulletcount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //move the ship w/left and right arrows
+            KeyEventArgs key = e;
+
+            if (key.KeyValue == 37)
+            {
+                lblspaceship.Left -= 10;
+            }
+
+            else if (key.KeyValue == 39)
+            {
+
+                lblspaceship.Left += 10;
+            }
+
+            if (key.KeyValue == 32)
+            {
+                if (bulletcount < 10)
+                {
+                    bulletlist.Add(new PictureBox());
+                    //place picbox on the form
+                    this.Controls.Add(bulletlist.ElementAt(bulletcount));
+                    bulletlist.ElementAt(bulletcount).Height = 10;
+                    bulletlist.ElementAt(bulletcount).Width = 10;
+                    bulletlist.ElementAt(bulletcount).Image = Image.FromFile("bulletbills.png", true);
+                    bulletlist.ElementAt(bulletcount).SizeMode = PictureBoxSizeMode.StretchImage;
+                    bulletlist.ElementAt(bulletcount).Left = lblspaceship.Left + lblspaceship.Width / 2;
+                    bulletlist.ElementAt(bulletcount).Top = lblspaceship.Top - 10;
+                    bulletcount++;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            movebullets();
+        }
+
+        private void movebullets()
+        {
+            for (int i = 0; i < bulletlist.Count; i++)
+            {
+                bulletlist.ElementAt(i).Top -= 10;
+            }
+
+            for (int j = 0; j < bulletlist.Count; j++)
+            {
+                if (lbltarget.Bounds.IntersectsWith(bulletlist.ElementAt(j).Bounds))
+                {
+                    lbltarget.Width -= 10;
+                }
+            }
+
+            //remove bullets from offstage
+            for (int k = 0; k < bulletlist.Count; k++)
+            {
+                if (bulletlist.ElementAt(k).Top < 0)
+                {
+                    this.Controls.Remove(bulletlist.ElementAt(k));
+                    bulletlist.Remove(bulletlist.ElementAt(k));
+                    bulletcount--;
+
+                    if (bulletcount <= 0)
+                    {
+                        bulletlist = new List<PictureBox>();
+                        bulletcount = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+    
+
